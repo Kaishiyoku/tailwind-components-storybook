955fb6effa48eb5013eb3ff14c5d9d1842635b71
Kaishiyoku <dmp@nfze.de>
2021-02-18 20:31:42 +0100

    minor preview adjustments

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,4 +2,10 @@ import "../src/index.css";
 
 export const parameters = {
     actions: {argTypesRegex: "^on[A-Z].*"},
+    html: {
+        highlighter: {
+            showLineNumbers: true,
+            wrapLines: true,
+        },
+    },
 };
